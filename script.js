f0ebb30c65a4c195b8b08bfffaf67b0084630a0d
Bhavna1312 <bhavnarajput973@gmail.com>
2025-02-26 22:36:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Use logical operators to find whether the age of a person lies between 10 and 20?
-/* let age = prompt("What is your age?")
+let age = prompt("What is your age?")
 if(age>10 && age<20){
     console.log("Your age lies between 10 and 20");
 }
 else{
     console.log("Your age doesnt lies between 10 and 20");
-} */
+}
 
 //Demonstrate the use of switch case statements in javascript
 /* let age = prompt("What is your age?")
