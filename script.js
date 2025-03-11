9e6b9bcfe75367309988dbb23f999b1d074d0b84
Bhavna1312 <bhavnarajput973@gmail.com>
2025-03-11 22:52:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-/* Write a program to print the marks of a student in an object using for loop
-obj = { harry: 98, rohan : 70, aakash: 7} */
-let marks = {
-  harry: 98,
-  rohan: 70,
-  aakash: 7,
-};
-for (let i = 0; i < Object.keys(marks).length; i++) {
-  console.log(
-    "The marks of" +
-      Object.keys(marks)[i] +
-      "are" +
-      marks[Object.keys(marks)[i]]
-  );
+//Use logical operators to find whether the age of a person lies between 10 and 20?
+/* let age = prompt("What is your age?")
+if(age>10 && age<20){
+    console.log("Your age lies between 10 and 20");
 }
+else{
+    console.log("Your age doesnt lies between 10 and 20");
+} */
 
-//Write the program in Q1 using for in loop
-for (let i in marks) {
-  console.log(i);
+//Demonstrate the use of switch case statements in javascript
+/* let age = prompt("What is your age?")
+switch (age) {
+    case 12:
+        console.log("Your age is 12");
+        break;
+    case 13:
+        console.log("Your age is 13");
+        break;
+    case 14:
+        console.log("Your age is 14");
+        break;
+    case 15:
+        console.log("Your age is 15");
+default:
+    console.log("Your age is not special");
+} */
+
+//Write a Javascript program to find whether a no. is Divisible by 2 and 3.
+/* let num = prompt("What is your age?")
+num = Number.parseInt(num)
+if (num % 2 == 0 && num % 3 == 0){
+    console.log("Your number is divisible by 2 and 3 ");
 }
+else {
+    console.log("Your number is not divisible by 2 and 3");
+    
+} */
+
+/*let num = prompt("What is your age?");
+num = Number.parseInt(num);
+if (num % 2 == 0) {
+  console.log("Your number is divisible by 2");
+} else {
+  num % 3 == 0;
+  console.log("Your number is divisible by 3");
+}
+*/
+let age = 12;
+let a = age > 18 ? "You can drive" : "You cannot drive";
+console.log(a);
